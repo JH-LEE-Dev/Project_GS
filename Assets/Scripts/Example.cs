8d3aacd6560d87483ba42e8b784401e9aff5ac83
Raven <90697588+Rainsang@users.noreply.github.com>
2025-10-22 15:42:17 +0900

    ▽ 깃 허브 테스트

---
+++ Assets/Scripts/Example.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class Example : MonoBehaviour
+{
+    [SerializeField] private string gunName;
+    [SerializeField] private int Ammo;
+    [SerializeField] private int maxAmmo;
+}
