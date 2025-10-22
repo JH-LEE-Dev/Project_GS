c5f316bd016e4e6d59c3b137249acddf352e7b6a
JH-LEE-Dev <140565804+JH-LEE-Dev@users.noreply.github.com>
2025-10-22 15:52:32 +0900

    Adding Some Data To Example Script.
    .

--- Assets/Scripts/Example.cs
+++ Assets/Scripts/Example.cs
@@ -3,6 +3,12 @@ using UnityEngine;
 public class Example : MonoBehaviour
 {
     [SerializeField] private string gunName;
-    [SerializeField] private int Ammo;
+    [SerializeField] private int ammo;
     [SerializeField] private int maxAmmo;
+    [SerializeField] private int level;
+
+    public void Awake()
+    {
+        //Empty Yet.
+    }
 }
