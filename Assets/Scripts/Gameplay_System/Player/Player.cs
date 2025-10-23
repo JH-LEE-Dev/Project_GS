6417b83715c439a044d370b8cad7973ab00ba529
Raven <90697588+Rainsang@users.noreply.github.com>
2025-10-23 17:12:53 +0900

    🔶Adding PlayerInputSet

--- Assets/Scripts/Gameplay_System/Player/Player.cs
+++ Assets/Scripts/Gameplay_System/Player/Player.cs
@@ -1,6 +1,33 @@
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-   
+    private PlayerInputSet input;
+
+    private void Awake()
+    {
+        input??= new PlayerInputSet();
+    }
+
+    private void Start()
+    {
+
+    }
+
+    private void OnEnable()
+    {
+        input.Enable();
+    }
+
+    private void OnDisable()
+    {
+        input?.Disable();
+    }
+
+    private void Update()
+    {
+
+    }
 }
