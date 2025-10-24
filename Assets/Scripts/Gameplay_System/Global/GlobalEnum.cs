57853e5e2ab741b00f39ba0ef7d9b1092019a7bc
Raven <90697588+Rainsang@users.noreply.github.com>
2025-10-24 19:51:51 +0900

    🔶 enum 오류 수정

--- Assets/Scripts/Gameplay_System/Global/GlobalEnum.cs
+++ Assets/Scripts/Gameplay_System/Global/GlobalEnum.cs
@@ -1,4 +1,9 @@
 using UnityEngine;
 
 public enum GunName { m92, revolver, sawedOffShotgun, ak47, mp5, m24, end }
+
 public enum GrabState { grabbed, dropped, end }
+
+public enum EnemyType { ZombieMen, ZombieMen2, ZombieGirl1, ZombieGirl2 };
+
+public enum EnemyAnimState { Idle, Move, Attack, Die }
