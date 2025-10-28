e425a7a92c157e6e5bbd89dbb050d065b9bb9a0d
Raven <90697588+Rainsang@users.noreply.github.com>
2025-10-28 16:34:18 +0900

    🔶 Projectile Custom CCD 로직 추가

--- Assets/Scripts/Gameplay_System/Gun/Projectile.cs
+++ Assets/Scripts/Gameplay_System/Gun/Projectile.cs
@@ -1,30 +1,34 @@
 using UnityEngine;
 using Game.GlobalFunc;
+using System.Runtime.CompilerServices;
+using System.Collections.Generic;
 
 public class Projectile : Entity
 {
     SpriteRenderer sr;
 
-    [Header("CCD Details")]
-    CapsuleCollider2D capCol;
-
     [Header("Target Details")]
     [SerializeField] private LayerMask targetLayer;
     [SerializeField] private float suicideDistance = 14f;
+
+    [Header("Summoner Details")]
+    Transform summoner;
     private float speed;
     private float damage;
     private int penCount;
 
-    Transform summoner;
-
-    private readonly RaycastHit2D[] hits = new RaycastHit2D[20];
-    private Vector2 prevPos;
+    [Header("CCD Details")]
+    static private readonly RaycastHit2D[] hits = new RaycastHit2D[16];
+    CapsuleCollider2D capCol;
+    ContactFilter2D contactFilter;
 
     private void Awake()
     {
         rb ??= GetComponent<Rigidbody2D>();
         capCol ??= GetComponent<CapsuleCollider2D>();
         sr ??= GetComponentInChildren<SpriteRenderer>();
+        
+        contactFilter = new ContactFilter2D { useLayerMask = true, layerMask = targetLayer, useTriggers = true };
     }
 
     private void FixedUpdate()
@@ -32,39 +36,12 @@ public class Projectile : Entity
         if (null == rb)
             return;
 
-        rb.linearVelocity = transform.right * 13f;//speed;
+        ContinuousColliderDetection();
     }
 
     private void Update()
     {
         SuicideDistanceCheck();
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (null == collision)
-            return;
-
-        GameObject colObj = collision.gameObject;
-        int targetMask = colObj.layer;
-
-        if (!LayerUtill.CompareLayerMask(colObj, targetLayer))
-            return;
-
-        IDamageable damageable = colObj.GetComponent<IDamageable>();
-        if (null == damageable)
-            return;
-
-        if (0 < penCount)
-        {
-            --penCount;
-            damageable.TakeDamage(damage);
-            return;
-        }
-
-        // TODO :: 오브젝트 풀링
-        damageable.TakeDamage(damage);
-        Destroy(gameObject);
     }
 
     public void StraightToTarget(Transform summoner, float damage, int penCount, float Speed)
@@ -85,6 +62,7 @@ public class Projectile : Entity
         // ( 캐릭터가 죽어도 존재해야 하는데 없으면 버그임 )
         if (null == summoner)
         {
+            gameObject.SetActive(false);
             Destroy(gameObject);
             return;
         }
@@ -93,11 +71,82 @@ public class Projectile : Entity
 
         // TODO :: 오브젝트 풀링
         if (distance > suicideDistance)
+        {
+            gameObject.SetActive(false);
             Destroy(gameObject);
+        }
+    }
+
+    private void ContinuousColliderDetection()
+    {
+        // 고정 시간동안 이동한 거리가 0에 수렴하면 스윕할 필요가 없다.
+        Vector2 velocity = rb.linearVelocity;
+        float deltaDistance = velocity.magnitude * Time.fixedDeltaTime;
+        if (0 >= deltaDistance)
+            return;
+
+        int cnt = rb.Cast(velocity.normalized, contactFilter, hits, deltaDistance);
+
+        IComparer<RaycastHit2D> comparer = System.Collections.Generic.Comparer<RaycastHit2D>.Create(
+            (a, b) => a.distance.CompareTo(b.distance));
+
+        if (0 < cnt)
+        {
+            System.Array.Sort(hits, 0, cnt, comparer);
+
+            for (int i = 0; i < cnt; ++i)
+            {
+                RaycastHit2D hit = hits[i];
+                GameObject target = hit.rigidbody ? hit.rigidbody.gameObject : hit.collider.gameObject;
+                OnCollisionTriggerEvent(target);
+            }
+        }
+    }
+
+    private bool OnCollisionTriggerEvent(GameObject colObj)
+    {
+        if (null == colObj)
+            return false;
+
+        int targetMask = colObj.layer;
+
+        if (!LayerUtill.CompareLayerMask(colObj, targetLayer))
+            return false;
+
+        IDamageable damageable = colObj.GetComponent<IDamageable>();
+        if (null == damageable)
+            return false;
+
+        if (0 < penCount)
+        {
+            --penCount;
+            damageable.TakeDamage(damage);
+            return true;
+        }
+
+        // TODO :: 오브젝트 풀링
+        damageable.TakeDamage(damage);
+        gameObject.SetActive(false);
+        Destroy(gameObject);
+        return true;
+    }
+
+    private void AsSoonSummonedOverlapCheck()
+    {
+        Collider2D[] overlapped = new Collider2D[8];
+        int cnt = capCol.Overlap(contactFilter, overlapped);
+
+        for (int i = 0; i < cnt; ++i)
+        {
+            GameObject targetObj = overlapped[i].attachedRigidbody.gameObject ? 
+                overlapped[i].attachedRigidbody.gameObject : overlapped[i].gameObject;
+
+            OnCollisionTriggerEvent(targetObj);
+        }
     }
 
     private void OnEnable()
     {
-        prevPos = rb.position;
+        AsSoonSummonedOverlapCheck();
     }
 }
